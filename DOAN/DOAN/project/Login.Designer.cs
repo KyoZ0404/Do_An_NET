b6637225d47f45cb7a71bf6d3570f0c32ae7b11b
Admin <Admin@THAIBAO>
2021-12-15 18:51:02 +0700

    Add project files.

---
+++ DOAN/DOAN/project/Login.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace project
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.lblTaiKhoan = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.rdbCashier = new System.Windows.Forms.RadioButton();
+            this.rdbAdmin = new System.Windows.Forms.RadioButton();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTaiKhoan
+            // 
+            resources.ApplyResources(this.lblTaiKhoan, "lblTaiKhoan");
+            this.lblTaiKhoan.Name = "lblTaiKhoan";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.txtUsername, "txtUsername");
+            this.txtUsername.Name = "txtUsername";
+            // 
+            // lblMatKhau
+            // 
+            resources.ApplyResources(this.lblMatKhau, "lblMatKhau");
+            this.lblMatKhau.Name = "lblMatKhau";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.txtPassword, "txtPassword");
+            this.txtPassword.Name = "txtPassword";
+            // 
+            // rdbCashier
+            // 
+            resources.ApplyResources(this.rdbCashier, "rdbCashier");
+            this.rdbCashier.BackColor = System.Drawing.Color.Silver;
+            this.rdbCashier.Checked = true;
+            this.rdbCashier.Name = "rdbCashier";
+            this.rdbCashier.TabStop = true;
+            this.rdbCashier.UseVisualStyleBackColor = false;
+            // 
+            // rdbAdmin
+            // 
+            resources.ApplyResources(this.rdbAdmin, "rdbAdmin");
+            this.rdbAdmin.BackColor = System.Drawing.Color.Silver;
+            this.rdbAdmin.Name = "rdbAdmin";
+            this.rdbAdmin.UseVisualStyleBackColor = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.btnLogin, "btnLogin");
+            this.btnLogin.Image = global::project.Properties.Resources.icons8_login_32;
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.btnThoat, "btnThoat");
+            this.btnThoat.Image = global::project.Properties.Resources.icons8_close_window_32;
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::project.Properties.Resources.icons8_password_128;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmLogin
+            // 
+            this.AcceptButton = this.btnLogin;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.rdbAdmin);
+            this.Controls.Add(this.rdbCashier);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblMatKhau);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblTaiKhoan);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTaiKhoan;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblMatKhau;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.RadioButton rdbCashier;
+        private System.Windows.Forms.RadioButton rdbAdmin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ DOAN/DOAN/project/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace project
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin());
+            //Application.Run(new frmAddFood());
+            //Application.Run(new frmMain());
+            //Application.Run(new frmAdAccount());
+            // Application.Run(new frmAdCategory());
+            //Application.Run(new frmAdFood());
+            //Application.Run(new frmAdTables());
+            //Application.Run(new frmPay());
+            //Application.Run(new frmNhanvien());
+            //Application.Run(new Block());
+            //Application.Run(new ReFood());
+
+            //Application.Run(new reportNhanVien());
+            //Application.Run(new ReportMon());
+
+        }
+    }
+}
